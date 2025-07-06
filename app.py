1dd90124afb07eef693b9cb54ab574782c7b4ff6
root <root@ip-10-0-12-172.us-east-2.compute.internal>
2025-07-06 18:00:16 +0000

    1

---
+++ app.py
@@ -0,0 +1,15 @@
+# This is a simple Python program
+
+# Print a greeting
+print("Hello, World!")
+
+# Define two numbers
+a = 5
+b = 3
+
+# Add the numbers
+sum = a + b
+
+# Print the result
+print("The sum of", a, "and", b, "is", sum)
+
